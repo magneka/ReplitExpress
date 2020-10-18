433961f7cb90e24c6f056ce94f63f19dc3b33572
Magne Alvheim <magne@alvheim.com>
2020-10-18 22:25:25 +0000

    rest met

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const app = express();
 
+const Database = require("@replit/database")
+const db = new Database()
+db.set("1", {
+      firstname: 'Magne',
+      lastname: 'Alvheim'
+    }).then(() => {});
+db.set("2", {
+      firstname: 'Donald',
+      lastname: 'Trump'
+    }).then(() => {});
+
+
 app.get('/', (req, res) => {
     res.send('Now we are cooking');
 });
 
+app.get('/users', async (req, res) => {  
+  let result = await db.list()
+  res.json(result);  
+});
+
+
+app.get('/user/:id', async (req, res) => {
+  console.log(req.params.id)  
+  let result = await db.get(req.params.id)
+  res.json(result);  
+});
 
 app.listen(3000, () => console.log('Gator app listening on port 3000!'));

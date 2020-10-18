ea360d68e0e620be48e097bac891699884986123
Magne Alvheim <magne@alvheim.com>
2020-10-18 21:46:53 +0000

    Result string

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('An alligator approaches!');
+    res.send('Now we are cooking');
 });
+
 
 app.listen(3000, () => console.log('Gator app listening on port 3000!'));
